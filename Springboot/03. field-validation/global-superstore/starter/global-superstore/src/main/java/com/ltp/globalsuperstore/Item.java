f3c2016db6186311e78a8d11a59c6242e0fc8369
thoai240699 <thoai240699@gmail.com>
2025-05-04 18:09:25 +0700

    global-superstore validation form

--- Springboot/03. field-validation/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/Item.java
+++ Springboot/03. field-validation/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/Item.java
@@ -3,13 +3,29 @@ package com.ltp.globalsuperstore;
 import java.util.Date;
 import java.util.UUID;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+
 import org.springframework.format.annotation.DateTimeFormat;
+import javax.validation.constraints.NotNull;
 
 public class Item {
+    @NotBlank(message = "Vui lòng chọn loại hàng.")
     private String category;
+
+    @NotBlank(message = "Tên không thể để trống.")
     private String name;
+
+    @NotNull(message = "Giá hàng không được để trống.")
+    @Min(value = 0, message = "Giá hàng không thể âm")
     private Double price;
+
+    @NotNull(message = "Discount không được để trống.")
+    @Min(value = 0, message = "Discount không thể âm.")
     private Double discount;
+
+    @Past(message = "Ngày phải là ngày quá khứ.")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
     private String id;
@@ -18,6 +34,8 @@ public class Item {
     public Item() {
         this.id = UUID.randomUUID().toString();
     }
+    @NotBlank
+
 
     public String getId() {
         return this.id;
